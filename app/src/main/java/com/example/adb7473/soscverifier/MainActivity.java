4cadd585b00da9f966718f3b2fee1dc42f7abb0a
akashdeepb <akashdeepb.cs17@sahyadri.edu.in>
2018-11-25 01:19:59 +0530

    Fixed Issue #13 : Check if Network Connected

--- app/src/main/java/com/example/adb7473/soscverifier/MainActivity.java
+++ app/src/main/java/com/example/adb7473/soscverifier/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.adb7473.soscverifier;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -51,12 +54,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                //Check if Input is not empty field
-                if(!code.getText().toString().isEmpty()) {
-                    Request request = new Request(MainActivity.this, code.getText().toString(), lastVerified);
+                //Check if Network Connected
+                boolean connected = false;
+                ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+                if (connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
+                        connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+                    //we are connected to a network
+                    connected = true;
+                } else
+                    connected = false;
+
+                //Send Request only when Network Connected
+                if (connected) {
+                    //Check if Input is not empty field
+                    if (!code.getText().toString().isEmpty()) {
+                        Request request = new Request(MainActivity.this, code.getText().toString(), lastVerified);
+                    } else
+                        Toast.makeText(MainActivity.this, "Enter Code", Toast.LENGTH_LONG).show();
                 }
                 else
-                    Toast.makeText(MainActivity.this,"Enter Code",Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this,"Check Internet Connection",Toast.LENGTH_LONG).show();
             }
         });
 
